f3ea645a47853531dae4ba5d393d64aa62da6e29
Will McLean <willmcl@gmail.com>
2019-01-18 07:39:13 +1100

    Setup basic styles.

---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <main className="Content">
+                <h1>Protostart!</h1>
+                <p>For <a href="http://willmclean.net">Will</a> to begin React prototypes quickly.</p>
+            </main>
+        )
+    }
+}
+
+export default Content
---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+    render() {
+        return (
+            <nav className="Menu">
+                <ul>
+                    <li>Home</li>
+                    <li>Page</li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Menu
