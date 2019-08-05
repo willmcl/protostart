1f7fd053ae035b620d3a06792567ef1014e35683
Will McLean <willmcl@gmail.com>
2019-08-05 16:15:12 +1000

    Resolve #2. Add the checkbox list and checkbox components.

--- src/organisms/DashboardContent.js
+++ src/organisms/DashboardContent.js
@@ -4,7 +4,20 @@ class DashboardContent extends Component {
   render() {
     return (
       <main className="DashboardContent">
-        <h1>Dashboard</h1>
+        <h1>Heading One</h1>
+        <h2>Heading Two</h2>
+        <h3>Heading Three</h3>
+        <h4>Heading Four</h4>
+        <h5>Heading Five</h5>
+        <h6>Heading Six</h6>
+        <p>Paragraph text</p>
+        <ul>
+          <li>List item</li>
+          <li>List item</li>
+          <li>List item</li>
+          <li>List item</li>
+        </ul>
+
       </main>
     )
   }
